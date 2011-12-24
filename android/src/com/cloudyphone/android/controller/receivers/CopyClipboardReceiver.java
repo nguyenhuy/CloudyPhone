a046125b01d4f84b28f3b7eab69795fb3360597a
NguyenThanhHuy <allforone1511@gmail.com>
2011-12-24 22:55:01 +0700

    finished Copy Clipboard, Open webpage commands

--- android/src/com/cloudyphone/android/controller/receivers/CopyClipboardReceiver.java
+++ android/src/com/cloudyphone/android/controller/receivers/CopyClipboardReceiver.java
@@ -1,11 +1,9 @@
 package com.cloudyphone.android.controller.receivers;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.androidpcsuite.android.LaunchBrowserService;
-
 import android.content.Context;
-import android.content.Intent;
 import android.text.ClipboardManager;
 import android.text.TextUtils;
 
@@ -13,17 +11,18 @@ public class CopyClipboardReceiver extends PushReceiver {
 
 	@Override
 	public void execute(Context ctx, JSONObject data) {
-		String string = data.getString("message");
-		if (TextUtils.isEmpty(string)) {
-			// No message, return
-			return;
+		try {
+			String string = data.getString("message");
+
+			if (TextUtils.isEmpty(string)) {
+				// No message, return
+				return;
+			}
+
+			// Get clipboard manager and set the text to it
+			((ClipboardManager) ctx.getSystemService(Context.CLIPBOARD_SERVICE))
+					.setText(string);
+		} catch (JSONException e) {
 		}
-
-		// Get clipboard manager
-		ClipboardManager clipboard = (ClipboardManager) ctx
-				.getSystemService(Context.CLIPBOARD_SERVICE);
-
-		// set to clipboard
-		clipboard.setText(string);
 	}
 }
