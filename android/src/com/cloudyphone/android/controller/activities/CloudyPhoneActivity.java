7bdce41f32569331db63f1c882c52a9a9afc992c
Nguyen Thanh Huy <thanhhuy@cogini.com>
2012-01-18 13:13:18 +0700

    add AndEngine for animation in CloudyPhoneActivity

--- android/src/com/cloudyphone/android/controller/activities/CloudyPhoneActivity.java
+++ android/src/com/cloudyphone/android/controller/activities/CloudyPhoneActivity.java
@@ -1,8 +1,24 @@
 package com.cloudyphone.android.controller.activities;
 
-import android.app.Activity;
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.input.touch.TouchEvent;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,13 +27,11 @@ import com.cloudyphone.android.R;
 import com.cloudyphone.android.controller.push.PushManager;
 import com.parse.ParseUser;
 
-public class CloudyPhoneActivity extends Activity {
+public class CloudyPhoneActivity extends BaseGameActivity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		setContentView(R.layout.main);
 
 		subscribePush();
 	}
@@ -69,5 +83,72 @@ public class CloudyPhoneActivity extends Activity {
 		default:
 			return super.onOptionsItemSelected(item);
 		}
+	}
+
+	/**
+	 * AndEngine part to do the animation
+	 */
+	private int CAMERA_WIDTH = 480;
+	private int CAMERA_HEIGHT = 720;
+
+	private Camera mCamera;
+	private BitmapTextureAtlas mBitmapTextureAtlas;
+	private TextureRegion mFaceTextureRegion;
+
+	@Override
+	public Engine onLoadEngine() {
+		Display display = getWindowManager().getDefaultDisplay();
+		CAMERA_WIDTH = display.getWidth();
+		CAMERA_HEIGHT = display.getHeight();
+
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+
+		return new Engine(new EngineOptions(true, ScreenOrientation.PORTRAIT,
+				new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT),
+				this.mCamera));
+	}
+
+	@Override
+	public void onLoadResources() {
+		this.mBitmapTextureAtlas = new BitmapTextureAtlas(64, 64,
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("img/");
+		this.mFaceTextureRegion = BitmapTextureAtlasTextureRegionFactory
+				.createFromAsset(this.mBitmapTextureAtlas, this, "cloud.png",
+						0, 0);
+		this.mEngine.getTextureManager().loadTexture(this.mBitmapTextureAtlas);
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
+		final Scene scene = new Scene();
+		scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
+
+		final int centerX = (CAMERA_WIDTH - this.mFaceTextureRegion.getWidth()) / 2;
+		final int centerY = (CAMERA_HEIGHT - this.mFaceTextureRegion
+				.getHeight()) / 2;
+		final Sprite face = new Sprite(centerX, centerY,
+				this.mFaceTextureRegion) {
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+					final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+				this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2,
+						pSceneTouchEvent.getY() - this.getHeight() / 2);
+				return true;
+			}
+		};
+		face.setScale(4);
+		scene.attachChild(face);
+		scene.registerTouchArea(face);
+		scene.setTouchAreaBindingEnabled(true);
+
+		return scene;
+	}
+
+	@Override
+	public void onLoadComplete() {
+
 	}
 }
